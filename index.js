55accf66605b30c99d2af8107b81f8007d6321f8
Joseph <jsltran@gmail.com>
2022-01-10 14:51:34 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(arr, driver) {
+    // let arrLower = arr.map(driverName => driverName.toLowerCase()); 
+    let specificDrivers = arr.filter(person => person.toLowerCase() === driver.toLowerCase()
+    );
+    return specificDrivers
+  }; 
+
+  function fuzzyMatch(arr, letter) {
+    let approx = arr.filter(function (letter) {
+      if (arr.includes(letter) === true) {
+        return approx
+      }
+    })
+    // let specificDrivers = arr.filter(approx => approx.toLowerCase())
+    };
